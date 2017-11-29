5d816b48b88507be1c393377e08f530bf2d0dc28
dtsetse <dtsetterlund@gmail.com>
2017-11-29 15:02:47 -0800

    - Updated userVM -created user signIn/Create page -learned a valuable lesson

--- Project1Phase1/ViewModels/UserVM.cs
+++ Project1Phase1/ViewModels/UserVM.cs
@@ -9,12 +9,16 @@ namespace Project1Phase1.ViewModels
     public class UserVM
     {
         [DisplayName("User ID")]
-        public string user_id;
+        public string user_id { get; set; }
+        [DisplayName("Email")]
+        public string email { get; set; }
         [DisplayName("First Name")]
-        public string fName;
+        public string fName { get; set; }
         [DisplayName("Last Name")]
-        public string lName;
+        public string lName { get; set; }
         [DisplayName("Balance")]
-        public decimal balance;
+        public decimal balance { get; set; }
+        [DisplayName("Password")]
+        public string password { get; set; }
     }
 }
