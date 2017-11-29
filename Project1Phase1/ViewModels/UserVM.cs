d6763e4fc4cadb2a3ebc7cfd60d95bd1058e0fdc
dtsetse <dtsetterlund@gmail.com>
2017-11-29 11:55:07 -0800

    - created a ViewModels folder - created a basic UserVM (user view model)

---
+++ Project1Phase1/ViewModels/UserVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1Phase1.ViewModels
+{
+    public class UserVM
+    {
+        [DisplayName("User ID")]
+        public string user_id;
+        [DisplayName("First Name")]
+        public string fName;
+        [DisplayName("Last Name")]
+        public string lName;
+        [DisplayName("Balance")]
+        public decimal balance;
+    }
+}
