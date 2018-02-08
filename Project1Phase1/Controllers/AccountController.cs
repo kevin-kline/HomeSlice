b7d63d48b46b18f977b3695b7167216ed306f9cc
a01033657 <a01033657@DTC01L0382-26>
2018-02-08 15:28:54 -0800

    Comments to Register email confirmation

--- Project1Phase1/Controllers/AccountController.cs
+++ Project1Phase1/Controllers/AccountController.cs
@@ -235,9 +235,9 @@ namespace Project1Phase1.Controllers
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
-                    await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
+                    //await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
 
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation("User created a new account with password.");
