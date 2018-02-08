6540e3ff113cb15927561feadd8d264c19c812cc
a01033657 <a01033657@DTC01L0382-26>
2018-02-08 15:16:37 -0800

    Landing page for successful login and register attempts has been fixed. (if user is emailConfirmed, goes to homepage otherwise goes to JoinCreateHousehold page!)

--- Project1Phase1/Controllers/AccountController.cs
+++ Project1Phase1/Controllers/AccountController.cs
@@ -61,12 +61,14 @@ namespace Project1Phase1.Controllers
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var user = await _userManager.FindByEmailAsync(model.Email);
+                
                 var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    if (await _userManager.IsEmailConfirmedAsync(user)) {
+
+                    var user = await _userManager.FindByEmailAsync(model.Email);
+                    if (!await _userManager.IsEmailConfirmedAsync(user)) {
                         return RedirectToAction("JoinCreateHousehold", "Home");
                     }
                     else
