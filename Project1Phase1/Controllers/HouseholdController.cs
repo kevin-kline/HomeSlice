e55cdb0e2a00f28eff5b3ee18414902af5863e44
dtsetse <dtsetterlund@gmail.com>
2018-02-20 12:03:47 -0800

    -added empty Household controller -added empty Household Repo -added empty TransactionRepo

---
+++ Project1Phase1/Controllers/HouseholdController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project1Phase1.Controllers
+{
+    public class HouseholdController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
