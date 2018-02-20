a885359a3141736462e0048a7258e9f8aadb92af
Kevin <a01022225@DTC01L0382-24>
2018-02-20 13:38:19 -0800

    added extensionMethods class with getUserId() function

--- Project1Phase1/Repositories/RoomieRepo.cs
+++ Project1Phase1/Repositories/RoomieRepo.cs
@@ -21,5 +21,13 @@ namespace Project1Phase1.Repositories
             _context.Roommates.Add(r1);
             _context.SaveChanges();
         }
+
+        public Roommate GetRoommate(string appUserId)
+        {
+            return _context.Roommates.Where(r => r.RoommateId == appUserId)
+                .FirstOrDefault();
+        }
+        
+
     }
 }
